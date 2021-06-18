4c45d4e1b6b61df792ae370f13ebe189015444b7
22612 <22612@DESKTOP-11RE00B>
2021-06-18 09:53:40 +0800

    Inital commit

--- src/taskmanager/info/KalznCompiler.java
+++ src/taskmanager/info/KalznCompiler.java
@@ -1,5 +1,16 @@
 package taskmanager.info;
-
+/**
+ * 
+ * @author 22612
+ *
+ */
 public class KalznCompiler {
-
+	public final static short COMPILE=0, INTERPRETED = 1, COMPILE_THEN_INTERPRETED = 3;
+	// language  typ
+	e
+	private int compilerID;
+	private String compilerName;
+	private short languageCompilerMode;
+	
+	
 }
